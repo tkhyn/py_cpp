bdf0310f8635de67fb1840cfc3c5bee5879809bd
Thomas Khyn <thomas@ksytek.com>
2018-03-29 11:27:23 +1300

    clean(cmake): move all cmake files to cpp/.cmake folder

--- cpp/.cmake/config.cmake
+++ cpp/.cmake/config.cmake
@@ -12,3 +12,21 @@ string(TOUPPER "${CMAKE_BUILD_TYPE}" U_CMAKE_BUILD_TYPE)
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
+# when including files in the same directory we have to use ../.cmake
+# as the current working directory is the module's directory, where the
+# CMakeLists.txt file lies
+
+# run conan from cmake
+include(../.cmake/conan.cmake)
+conan_cmake_run(
+    CONANFILE conanfile.txt
+    BASIC_SETUP
+    BUILD missing
+)
+
+# pybind11 configuration
+include(../.cmake/pybind11.cmake)
+
+# post-build steps
+include(../.cmake/postbuild.cmake)
