fe8924ab3303f87f33a377c8758f4b1194830be2
Thomas Khyn <thomas@ksytek.com>
2018-03-29 15:20:00 +1300

    feat(setup): add extension builder

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
-from setuptools import setup, find_packages
 import os
+import re
+import sys
+import platform
+import subprocess
+
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
 
 
 APP_NAME = 'py_cpp'
+CPP_DIR = 'cpp'
 
 
 # imports versionning variables, avoiding to import the `app` module
@@ -20,6 +28,66 @@ DEV_STATUS = {
     'final': '5 - Production/Stable'
 }
 
+
+# from https://github.com/pybind/cmake_example
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError(
+                'CMake must be installed to build the following extensions: '
+                ', '.join(e.name for e in self.extensions)
+            )
+
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(
+                re.search(r'version\s*([\d.]+)', out.decode()).group(1)
+            )
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(
+            os.path.dirname(self.get_ext_fullpath(ext.name))
+        )
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_%s=%s'
+                           % (cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '%s -DVERSION_INFO=\\"%s\\"' % (
+            env.get('CXXFLAGS', ''), self.distribution.get_version()
+        )
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
+                              cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args,
+                              cwd=self.build_temp)
+
 setup(
     name=APP_NAME,
     version=__version__,
@@ -35,11 +103,15 @@ setup(
         'Development Status :: %s' % DEV_STATUS[dev_status],
         'Environment :: Console'
     ],
+    ext_modules=[CMakeExtension(d, CPP_DIR)
+                 for d in os.listdir(CPP_DIR) if not d.startswith('.')],
+    cmdclass=dict(build_ext=CMakeBuild),
     packages=find_packages(),
     install_requires=(),
     entry_points={
         'console_scripts': [
             '%s = app.main:run' % APP_NAME
         ],
-    }
+    },
+    zip_safe=False
 )
